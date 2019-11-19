46b2acdd8665fc17a58c16ea7e445d6acc814d69
Christopher <christopher@seekingwebdev.com>
2019-11-19 12:16:03 +0000

    style results page

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// set html view engine using ejs. NEED ALL 5 LINES
+const path = require('path');
+const ejs = require('ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use(express.urlencoded());
+
 app.use(express.static("public"));
 app.use(express.static("views"));
 
-app.get("/", (req, res) => res.send("index.html"));
+app.get("/", (req, res) => res.render("index.html"));
+app.get("/results", (req, res) => res.render("results.html"));
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
