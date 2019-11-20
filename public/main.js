a779745a9fe5a8b51bbb08c25ae7ada2bd6faf63
Christopher <christopher@seekingwebdev.com>
2019-11-20 09:41:01 +0000

    append keyword to url

--- public/main.js
+++ public/main.js
@@ -1,18 +1,18 @@
-console.log('does the jQuery file even work?!');
+// console.log('does the jQuery file even work?!');
 
-// $(document).ready(() => {
-//   $.get("../searchResults.txt", (data) => {
-//     const results = [];
-//     console.log('data should go here');
-//     console.log(data);
-//     // $.each(data, (key, val) => {
-//     //   results.push()
-//     // })
-//   }, "text");
-// });
+// // $(document).ready(() => {
+// //   $.get("../searchResults.txt", (data) => {
+// //     const results = [];
+// //     console.log('data should go here');
+// //     console.log(data);
+// //     // $.each(data, (key, val) => {
+// //     //   results.push()
+// //     // })
+// //   }, "text");
+// // });
 
-const file = 'searchResults.txt';
+// const file = 'searchResults.txt';
 
-fetch(file)
-  .then(res => res.text())
-  .then(text => console.log('text'));
+// fetch(file)
+//   .then(res => res.text())
+//   .then(text => console.log('text'));
